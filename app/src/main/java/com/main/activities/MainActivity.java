7ff45bebb70b2bad3bcf712e574f7035b02b3aca
walter1912 <thaiho2k@gmail.com>
2023-04-19 16:12:59 +0700

    thêm giao diện

--- app/src/main/java/com/main/activities/MainActivity.java
+++ app/src/main/java/com/main/activities/MainActivity.java
@@ -1,16 +1,171 @@
 package com.main.activities;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import com.main.R;
+import com.main.database.DatabaseHelper;
+
 
 public class MainActivity extends AppCompatActivity {
+    private DatabaseHelper mDatabaseHelper;
+    private Button mButtonFavorite, mButtonGame, mButtonLearn, mButtonFloatingWindow;
+    private SimpleCursorAdapter dataAdapter;
+    private Button mNewWord;
+    ListView listViewNewWord;
+    private ImageView mPreviewIv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //hide actionBar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        mPreviewIv = findViewById(R.id.imageIv);
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Jua-Regular.ttf");
+        TextView mAppName = findViewById(R.id.text_app_name);
+        mAppName.setTypeface(typeface);
+
+        mDatabaseHelper = new DatabaseHelper(this);
+        mDatabaseHelper.createDataBase();
+
+        mButtonGame = findViewById(R.id.btn_game_main);
+        mButtonGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mDatabaseHelper.getAllFavWord().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Phải chọn từ yêu thích trước", Toast.LENGTH_LONG).show();
+                } else {
+                    Intent i = new Intent(MainActivity.this, StartGameScreenActivity.class);
+                    startActivity(i);
+                }
+            }
+        });
+
+        mButtonLearn = findViewById(R.id.btn_learn_word);
+        mButtonLearn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, StartingScreenActivity.class);
+                startActivity(i);
+            }
+        });
+
+        mButtonFavorite = findViewById(R.id.btn_fav_word);
+        mButtonFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, FavoriteActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            }
+        });
+
+        mButtonFloatingWindow = findViewById(R.id.btn_looking);
+        getPermission();
+        mButtonFloatingWindow.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+                if (!Settings.canDrawOverlays(MainActivity.this)) {
+                    getPermission();
+                } else {
+                    Intent intent = new Intent(MainActivity.this, FloatingWindow.class);
+                    startService(intent);
+                    finish();
+                }
+            }
+        });
+
+        mNewWord = findViewById(R.id.new_word_refresh);
+        mNewWord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayListView();
+            }
+        });
+        displayListView();
     }
+
+    //Display word in an activity
+    public void getDescription(String word) {
+        String ans = mDatabaseHelper.getDescription(word);
+        Intent intent = new Intent(MainActivity.this, DisplayWordActivity.class);
+        intent.putExtra("word", word);
+        intent.putExtra("answer", ans);
+        startActivity(intent);
+    }
+
+    private void displayListView() {
+        Cursor cursor = mDatabaseHelper.getNewWord();
+
+        // The desired columns to be bound
+        String[] columns = new String[] {
+                DatabaseHelper.WORD,
+                DatabaseHelper.DES
+        };
+
+        // the XML defined views which the data will be bound to
+        int[] to = new int[] {
+                R.id.text_view_eng_word,
+                R.id.text_view_description,
+        };
+
+        // create the adapter using the cursor pointing to the desired data
+        //as well as the layout information
+        dataAdapter = new SimpleCursorAdapter(
+                this, R.layout.activity_listview,
+                cursor,
+                columns,
+                to,
+                0);
+
+        listViewNewWord = (ListView) findViewById(R.id.list_new_word);
+        // Assign adapter to ListView
+        listViewNewWord.setAdapter(dataAdapter);
+
+        listViewNewWord.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //get item's position and retrieve it from db
+                Object clickItemObject = parent.getAdapter().getItem(position);
+                SQLiteCursor c = (SQLiteCursor)clickItemObject;
+                String word= c.getString(c.getColumnIndex(DatabaseHelper.WORD));
+                getDescription(word);
+            }
+        });
+    }
+
+
+    //Floating window
+    public void getPermission(){
+        //check for alert windows permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)){
+            Intent intent= new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent,1);
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!Settings.canDrawOverlays(this)) {
+                    Toast.makeText(this, "Permission denied by user", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+}
 }
---
+++ app/src/main/java/com/main/database/DatabaseHelper.java
@@ -0,0 +1,267 @@
+package com.main.database;
+
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.text.Html;
+import android.util.Log;
+import com.main.models.Question;
+import com.main.models.Word;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+import static android.content.ContentValues.TAG;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final int VERSION =1;
+    private static final String DB_NAME ="dict.db";
+    private static final String AV_TABLE = "av";
+    public static final String WORD = "word";
+    public static final String DES = "description";
+    private static String DB_PATH = "";
+    private final Context mContext;
+    private static SQLiteDatabase mDataBase;
+
+    public DatabaseHelper(Context context){
+        super(context,DB_NAME, null, VERSION );
+        if (android.os.Build.VERSION.SDK_INT >= 17) {
+            DB_PATH = context.getApplicationInfo().dataDir + "/databases/";
+        } else {
+            DB_PATH = "/data/data/" + context.getPackageName() + "/databases/";
+        }
+        this.mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+    public void createDataBase(){
+        //If the database does not exist, copy it from the assets.
+
+        boolean mDataBaseExist = checkDataBase();
+        if (!mDataBaseExist) {
+            this.getReadableDatabase();
+            this.close();
+            try {
+                //Copy the database from assests
+                copyDataBase();
+                Log.e(TAG, "createDatabase database created");
+            } catch (IOException mIOException) {
+                throw new Error("ErrorCopyingDataBase");
+            }
+        }
+    }
+
+    private void copyDataBase() throws IOException {
+        InputStream mInput = mContext.getAssets().open(DB_NAME);
+        String outFileName = DB_PATH + DB_NAME;
+        OutputStream mOutput = new FileOutputStream(outFileName);
+        byte[] mBuffer = new byte[1024];
+        int mLength;
+        while ((mLength = mInput.read(mBuffer)) > 0) {
+            mOutput.write(mBuffer, 0, mLength);
+        }
+        mOutput.flush();
+        mOutput.close();
+        mInput.close();
+    }
+
+    private boolean checkDataBase() {
+        File dbFile = new File(DB_PATH + DB_NAME);
+        return dbFile.exists();
+    }
+
+    //Open the database, so we can query it
+    public boolean openDataBase() throws SQLException {
+        String mPath = DB_PATH + DB_NAME;
+        //Log.v("mPath", mPath);
+        mDataBase = SQLiteDatabase.openDatabase(mPath, null, SQLiteDatabase.CREATE_IF_NECESSARY);
+        //mDataBase = SQLiteDatabase.openDatabase(mPath, null, SQLiteDatabase.NO_LOCALIZED_COLLATORS);
+        return mDataBase != null;
+    }
+
+    @Override
+    public synchronized void close() {
+        if (mDataBase != null)
+            mDataBase.close();
+        super.close();
+    }
+
+    //Get English words that like the input word
+    public ArrayList<String> getEngWord(String query){
+        ArrayList<String> engList= new ArrayList<>();
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        //truy vấn ra những từ gần giống theo từ mình search
+        Cursor cursor = sqLiteDatabase.rawQuery(
+//                AV_TABLE,
+//                new String[]{WORD},
+//                WORD + " LIKE ?",
+//                new String[]{"%" + query + "%"},
+//                null, null,
+//                WORD
+//                AV_TABLE,
+//                new String[]{WORD},
+//                WORD + " LIKE ?",
+//                new String[]{query + "%"},
+//                null, null,
+//                null
+                "SELECT word FROM av WHERE word LIKE '" + query + "%'", null
+        );
+        int index = cursor.getColumnIndex(WORD);
+        while (cursor.moveToNext()){
+            engList.add(cursor.getString(index));
+        }
+        sqLiteDatabase.close();
+        cursor.close();
+        return engList;
+    }
+
+    //Display word by html column
+    public Word displayWord(String word) {
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+
+        Word ans = new Word();
+        String sql = "SELECT * FROM " + AV_TABLE + " WHERE word = '" + word + "'";
+        Cursor cursor = sqLiteDatabase.rawQuery(sql, null);
+        while (cursor.moveToNext()) {
+            ans.setId(cursor.getInt(cursor.getColumnIndex("id")));
+            ans.setHtml(cursor.getString(cursor.getColumnIndex("html")));
+            ans.setWord(cursor.getString(cursor.getColumnIndex("word")));
+            String fav = cursor.getString(cursor.getColumnIndex("fav"));
+            if(fav.equals("TRUE")){
+                ans.setFav(true);
+            }else{
+                ans.setFav(false);
+            }
+        }
+        return ans;
+    }
+
+    //Display word by html column _ p2
+    public String getDescription(String word){
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        String ans = null;
+        String sql = "SELECT html FROM "+ AV_TABLE + " WHERE word = '"+ word + "'";
+        Cursor cursor = sqLiteDatabase.rawQuery(sql, null);
+        while (cursor.moveToNext()){
+            ans = cursor.getString(cursor.getColumnIndex("html"));
+        }
+        //nếu phải cần dùng flags thì dùng Html.FROM_HTML_MODE_COMPACT
+        //return Html.fromHtml(ans, Html.FROM_HTML_MODE_COMPACT).toString();
+        return Html.fromHtml(ans).toString();
+    }
+
+    //Get all the whole favorite word
+    public ArrayList<Word> getAllFavWord(){
+        ArrayList<Word> favWordList = new ArrayList<>();
+        ;
+        mDataBase = this.getReadableDatabase();
+        try {
+            Cursor cursor = mDataBase.rawQuery("SELECT * FROM av WHERE fav = 'TRUE'", null);
+            if (cursor.moveToFirst()){
+                do {
+                    Word word = new Word();
+                    word.setId(cursor.getInt(cursor.getColumnIndex("id")));
+                    word.setWord(cursor.getString(cursor.getColumnIndex("word")));
+                    word.setHtml(cursor.getString(cursor.getColumnIndex("html")));
+                    word.setDescription(cursor.getString(cursor.getColumnIndex("description")));
+                    String fav = cursor.getString(cursor.getColumnIndex("fav"));
+                    if(fav.equals("TRUE")){
+                        word.setFav(true);
+                    }else{
+                        word.setFav(false);
+                    }
+                    favWordList.add(word);
+                }while (cursor.moveToNext());
+            }
+            cursor.close();
+        }catch (Exception e){        }
+
+        return favWordList;
+    }
+
+    //biến id là id của word được chọn yêu thích
+    public void updateFav(boolean checked, int id){
+        mDataBase = this.getReadableDatabase();
+        ContentValues contentValues = new ContentValues();
+        if(checked){
+            //từ đang ở trạng thái được yêu thích
+            //khi click vào view phải chuyển thành ko yêu thích là false
+            contentValues.put("fav", "FALSE");
+        }else{
+            contentValues.put("fav", "TRUE");
+        }
+        mDataBase.update("av", contentValues, "id = " + id,
+                null);
+    }
+
+    ////Get English FAVORITE words that like the input word
+    public ArrayList<String> getFavEngWord(String query){
+        ArrayList<String> engList= new ArrayList<>();
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        //truy vấn ra những từ gần giống theo từ mình search
+        Cursor cursor = sqLiteDatabase.query(
+                AV_TABLE,
+                new String[]{WORD},
+                WORD + " LIKE ? AND fav = 'TRUE'" ,
+                new String[]{query + "%"},
+                null, null,
+                WORD
+        );
+        int index = cursor.getColumnIndex(WORD);
+        while (cursor.moveToNext()){
+            engList.add(cursor.getString(index));
+        }
+        sqLiteDatabase.close();
+        cursor.close();
+        return engList;
+    }
+
+    //Quiz game
+    public ArrayList<Question> getAllQuestion(int number){
+        ArrayList<Question> questionList = new ArrayList<Question>(number);
+        mDataBase= this.getReadableDatabase();
+        Cursor c= mDataBase.rawQuery("SELECT * FROM practice",null);
+
+        if (c.moveToFirst()){
+            do {
+                Question question= new Question();
+                question.setCauhoi(c.getString(c.getColumnIndex("question")));
+                question.setCaseA(c.getString(c.getColumnIndex("caseA")));
+                question.setCaseB(c.getString(c.getColumnIndex("caseB")));
+                question.setCaseC(c.getString(c.getColumnIndex("caseC")));
+                question.setTrueCase(c.getInt(c.getColumnIndex("true_case")));
+                questionList.add(question);
+            }while (c.moveToNext());
+        }
+        c.close();
+        return questionList;
+    }
+
+    //New word - new day
+    public Cursor getNewWord() {
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        Cursor mCursor = sqLiteDatabase.query(AV_TABLE, new String[] {"rowid _id",WORD,DES},
+                null, null, null, null,"RANDOM()", String.valueOf(3));
+
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+    }
+}
